e266f28bf0c3813b282950c7b61b20eaffc79e45
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2017-10-19 20:27:16 -0400

    add in solution

--- starter/level-one.js
+++ starter/level-one.js
@@ -2,3 +2,20 @@
  * DOTS: Level One
  *
  */
+let score = 0
+let ballEl = document.querySelector('.js-ball')
+let scoreEl = document.querySelector('.js-score')
+
+ballEl.addEventListener('click', () => {
+  score += 10
+
+  if ( score < 100 ) {
+    scoreEl.innerText = score
+  } else {
+    declareWinner()
+  }
+})
+
+function declareWinner() {
+  document.body.classList.add('game-over')
+}
